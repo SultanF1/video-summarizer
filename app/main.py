17e234391593946d235d3de6142820f76a0ec561
SultanRazin <92024243+SultanF1@users.noreply.github.com>
2022-06-20 20:13:50 +0300

    first commit

--- app/main.py
+++ app/main.py
@@ -1,7 +1,83 @@
-from flask import Flask
+from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import pipeline
+from flask import Flask, request, jsonify,render_template
+from flask_cors import CORS
+
+import transformers
+
+
+
+
+
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def home_view():
-        return "<h1>Hello World!</h1>"
+
+cors = CORS(app)
+
+
+
+
+
+def generate_transcript(url):
+    id = url[url.index("=")+1:]
+        
+    transcript = YouTubeTranscriptApi.get_transcript(id)
+    script = ""
+
+    for text in transcript:
+        t = text["text"]
+        if t != '[Music]':
+            script += t + " "
+		
+    return script
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/post", methods=["POST"])
+def getME():
+    url = request.get_json()['mes']
+    print(url)
+    summarization = pipeline(model='sshleifer/distilbart-cnn-12-6')
+    res = summarization(generate_transcript(url), truncation=True)[0]['summary_text']
+    print(res)
+    return jsonify(res)
+
+
+
+@app.route("/rec", methods=["GET","POST"])
+def postME():
+    summarization = pipeline(model='sshleifer/distilbart-cnn-12-6')
+    res = summarization(generate_transcript('https://www.youtube.com/watch?v=0oBi8OmjLIg'), truncation=True)[0]['summary_text']
+    return res
+    
+    
+
+    
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
